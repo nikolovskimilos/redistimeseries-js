7c3b9da2d0f49da40e72598f881deeb4e7215057
Milos Nikolovski <nikolovskimilos@gmail.com>
2020-01-18 12:39:38 +0100

    Modifying readme and examples

--- examples/example01.js
+++ examples/example01.js
@@ -1,4 +1,4 @@
-const { RedisTimeSeries } = require('../index');
+const RedisTimeSeries = require('../index');
 
 const rtsClient = new RedisTimeSeries();
 
@@ -6,12 +6,12 @@ const key = 'temperature';
 const retention = 60000;
 
 const updateTemperature = async () => {
-	await rtsClient.add(key, Date.now(), Math.floor(Math.random()*30));
+	await rtsClient.add(key, Date.now(), Math.floor(Math.random()*30)).send();
 }
 
 const start = async () => {
 	await rtsClient.connect();
-	await rtsClient.create(key, { retention, uncompressed: true });
+	await rtsClient.create(key).retention(retention).uncompressed().send();
 	setInterval(updateTemperature, 1000);
 }
 
